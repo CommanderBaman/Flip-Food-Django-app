b06bd8c5637e84c6f4c55ec9a9cb51f9a8ee8203
Shubham Ojha <63144588+CommanderBaman@users.noreply.github.com>
2020-08-10 17:23:21 +0530

    added food models and its objects;

--- food_delivery/models.py
+++ food_delivery/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+# this contains all the food items that can be bought
+class Food( models.Model):
+    # name and description of food
+    name = models.CharField( max_length= 75)
+    description = models.CharField( max_length= 200)
+
+    # making images required - Pillow needs to installed
+    image = models.ImageField( blank= False, null= True, upload_to= 'foods')
+
+    # MRP of the item
+    price = models.DecimalField( max_digits= 5, decimal_places= 2)
+
+    # discount on the item
+    discount = models.DecimalField( max_digits= 5, decimal_places= 2)
+
+    # quantity available
+    quantity = models.IntegerField( default= 0)
+
+    @property
+    def isAvailable( self):
+        return self.quantity > 0
+
+    def __str__(self):
+        return self.name
+        # return '{}: {}\nAvailable for Rs.{} after discount of Rs.{}'.format( self.name, self.description, self.price - self.discount, self.discount)
+    
+    
--- ls_webdev_project/settings.py
+++ ls_webdev_project/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+# user defined settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
