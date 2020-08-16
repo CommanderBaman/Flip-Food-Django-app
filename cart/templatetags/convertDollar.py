94adf3af58cc11bee1410b4608ffd73f7958d475
Shubham Ojha <63144588+CommanderBaman@users.noreply.github.com>
2020-08-16 11:36:50 +0530

    applied basic payment api;

--- cart/templatetags/convertDollar.py
+++ cart/templatetags/convertDollar.py
@@ -1,8 +1,10 @@
 from django import template
+import numpy as np
+import decimal
 
 register = template.Library()
 
 @register.filter
 def convertDollar( quotient):
-    return quotient/69
+    return round( quotient/69, 2) # assuming the dollar rate to be 69
 
