8889e0485ec59e22a8804fed068bf0600f168828
Shubham Ojha <63144588+CommanderBaman@users.noreply.github.com>
2020-08-16 14:59:52 +0530

    checkpoint for before address migration; added toggle on user detail page;

--- users/models.py
+++ users/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
-
 from PIL import Image
+
+import json, requests
 
 class Profile( models.Model):
     user = models.OneToOneField( User, on_delete= models.CASCADE)
@@ -19,5 +20,31 @@ class Profile( models.Model):
         # resizing image to fit the template better
         imageSaved = Image.open( self.image.path)
         outputSize = ( 360, 360 * imageSaved.height / imageSaved.width)
-        imageSaved.thumbnail( outputSize)
+        imageSaved.resize( outputSize)
         imageSaved.save( self.image.path)
+
+
+class Address( models.Model):
+    # linking to profile
+    profile = models.ForeignKey( Profile, on_delete= models.CASCADE)
+    # loading cities
+    cityJSONfile = 'https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json'
+    res = requests.get( cityJSONfile)
+    cityDict = json.loads( res.text)
+    cityChoices = [( str( city["name"]), str( city["state"])) for city in cityDict]
+
+    city = models.CharField( max_length= 120, choices= cityChoices)
+
+    # loading states
+    stateChoices = [( str( state["state"]), "India") for state in cityDict]
+    state = models.CharField( max_length= 120, choices= stateChoices)
+
+    # PIN number of the city the person is living in
+    pin = models.IntegerField()
+
+    # address line
+    localityAddress = models.TextField( max_length= 180)
+
+    def __str__(self):
+        return "{}'s Address - Line {}".format( self.profile.user.username, self.id)
+
